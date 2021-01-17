9d45e00470302194cbfe7463e3f9b7a88fe4ea38
Masud Abdullah Al <abdullahalmasud.buet@gmail.com>
2021-01-17 13:43:00 +0900

    restating msd import in mlutils.py

--- msdlib/mlutils.py
+++ msdlib/mlutils.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 import pandas as pd
 import matplotlib.pyplot as plt
-import msd
+from msdlib import msd
 import numpy as np
 plt.rcParams['figure.facecolor'] = 'white'
 
