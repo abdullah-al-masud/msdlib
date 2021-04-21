55c1130363c68c7fb047b0717beca742cd8a5d7f
Masud Abdullah Al <abdullahalmasud.buet@gmail.com>
2021-04-22 04:13:48 +0900

    updating mlutils with simplification in NN model construction

--- msdlib/mlutils.py
+++ msdlib/mlutils.py
@@ -4,10 +4,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from msdlib import msd
 import numpy as np
+import os
+import joblib
 plt.rcParams['figure.facecolor'] = 'white'
 
 
 class NNmodel(nn.Module):
+    """
+    layer_funcs: list, contains sequential layer classes (nn.Module). For example-
+                    [nn.Linear(50), nn.ReLU(), nn.Linear(3), nn.Softmax(dim=-1)]
+    seed_value: float/int, random seed for reproducibility
+    """
     def __init__(self, layer_funcs, seed_value=1216):
         
         super(NNmodel, self).__init__()
@@ -20,6 +27,9 @@ class NNmodel(nn.Module):
         self.layers = nn.ModuleList(layer_funcs)
         
     def forward(self, x):
+        """
+        pytorch forward function for forward propagation
+        """
         for layer in self.layers:
             x = layer(x)
         return x
@@ -30,6 +40,7 @@ class AutoEncoderModel(nn.Module):
     """
     enc_layers: python list, containing the encoder layers (torch.nn.Module class objects) sequentially
     dec_layers: python list, containing the decoder layers (torch.nn.Module class objects) sequentially
+    seed_value: float/int, random seed for reproducibility
     """
     def __init__(self, enc_layers, dec_layers, seed_value=1216):
         
@@ -46,16 +57,28 @@ class AutoEncoderModel(nn.Module):
         self.dec_len = len(self.decode_layers)
     
     def encode(self, x):
-        for i in range(self.enc_len):
-            x = self.encode_layers[i](x)
+        """
+        Encoder part in Autoencoder model
+        x: input tensor for encoder part
+        """
+        for layer in self.encode_layers:
+            x = layer(x)
         return x
     
     def decode(self, x):
-        for i in range(self.dec_len):
-            x = self.decode_layers[i](x)
+        """
+        Decoder part in Autoencoder model
+        x: input tensor for decoder part
+        """
+        for layer in self.decode_layers:
+            x = layer(x)
         return x
         
     def forward(self, x):
+        """
+        pytorch forward function for forward propagation, applies encoder and then decoder sequentially on the input data
+        x: input tensor for autoencoder model 
+        """
         x = self.encode(x)
         x = self.decode(x)
         return x
@@ -193,15 +216,16 @@ class torchModel():
             
             # loss scheduler step
             self.scheduler.step()
-            
-            # run evaluation to get validation score
-            self.model.eval()
-            out = self.predict(val_data).squeeze()
-            val_loss = self.loss_func(out, val_label)
-            
             # storing losses
             loss_curves[0].append(np.mean(tr_mean_loss))
-            loss_curves[1].append(val_loss.item())
+            
+            if val_data.shape[0] > 0:
+                # run evaluation to get validation score
+                self.model.eval()
+                out = self.predict(val_data).squeeze()
+                val_loss = self.loss_func(out, val_label)
+                # storing losses
+                loss_curves[1].append(val_loss.item())
         
         print('...training complete !!')
         losses = pd.DataFrame(loss_curves, index = ['train_loss', 'validation_loss'], columns = np.arange(1, self.epoch + 1)).T.rolling(self.loss_roll_period).mean()
@@ -298,3 +322,83 @@ class torchModel():
             results = pd.concat(results, axis = 1, sort = False).T
 
         return results, all_results
+
+
+def get_factors(n_layers, base_factor=5, max_factor=10, offset_factor=2):
+    """
+    n_layers: number of hidden layers
+    offset_factor: makes assymetric structure (base - offset)
+    max_factor: multiplier for mid layer (largest layer)
+    base_factor: multiplier for first layer
+    """
+    base_factor = max_factor - base_factor
+    return [max_factor - abs(x) for x in np.linspace(-base_factor, base_factor + offset_factor, n_layers) if max_factor - abs(x) > 0]
+
+
+
+def define_layers(input_units, output_units, unit_factors, dropout_rate, model_type, actual_units=False):
+    """
+    input_units: int, number of units in input layer / number of features (not first hidden layer)
+    output_units: int, number of units in output layer / number of output nodes / number of classes (not last hidden layer)
+    unit_factors: array of ints or floats, multipliers to calculate number of units in each hidden layer from input_units, or actual number of units for each hidden layer
+    dropout_rate: dropout ratio
+    model_type: {'classifier', 'regressor'}, controls use of softmax on top of output layer
+    actual_units: bool, whether actual units are placed in unit_factors or not
+    """
+    if actual_units:
+        hidden_units = unit_factors.copy()
+    else:
+        hidden_units = [input_units * factor for factor in unit_factors]
+    units = [input_units] + hidden_units + [output_units]
+    units = [int(i) for i in units]
+    
+    layers = []
+    for i in range(len(unit_factors)):
+        layers.append(nn.Linear(units[i], units[i + 1]))
+        layers.append(nn.BatchNorm1d(units[i + 1]))
+        layers.append(nn.ReLU())
+        layers.append(nn.Dropout(dropout_rate))
+    layers.append(nn.Linear(units[-2], units[-1]))
+    if model_type == 'classifier':
+        layers.append(nn.Softmax(dim=1))
+    return layers
+
+
+# storing the models and loading them
+def store_models(models, folder_path):
+    """
+    models: dict, containing only trained model class; {<model name>: <model class>}
+            For pytorch models, the key must contain 'pytorch' phrase
+    folder_path: str, the folder path where the models will be stores, if doesnt exist, it will be created
+    """
+    if not os.path.exists(folder_path): os.makedirs(folder_path)
+    for modelname in models:
+        print('storing models... %s_model...'%modelname, end='')
+        if 'pytorch' in modelname.lower():
+            torch.save(models[modelname].model.state_dict(), folder_path + '/%s_model.pt'%modelname)
+        else:
+            with open(folder_path + '/%s_model.pickle'%modelname, 'wb') as f:
+                joblib.dump(models[modelname], f)
+        print('   ...storing completed !!')
+    
+
+def load_models(models, folder_path):
+    """
+    models: dict, containing model classes or None (for torch model, pytorch nn.Module class is necessary 
+            to load the state variables. For other types of models like xgboost etc. None is fine.);
+            For pytorch models, the key must contain 'pytorch' phrase
+            key name must be like this :
+            stored model file name: xgboost_model.pickle
+            corresponding key for the dict: 'xgboost'
+    folder_path: str, folder path from where the stored models will be loaded
+    """
+    for modelname in models:
+        print('\rloading models... %s_model...'%modelname, end='')
+        if 'pytorch' in modelname.lower():
+            models[modelname].model.load_state_dict(torch.load(folder_path + '/%s_model.pt'%modelname))
+        else:
+            with open(folder_path + '%s_model.pickle'%modelname, 'rb') as f:
+                models[modelname] = joblib.load(f)
+        print('   ...loading completed !!')
+    return models
+
